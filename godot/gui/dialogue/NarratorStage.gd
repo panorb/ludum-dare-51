06f215721bca5178d6bbda2b2e6685a9e8cea325
Paul Norberger <panor@fastmail.com>
2022-10-03 01:57:11 +0200

    Extended and improved dialogue system by quite a lot
    - chance branches are now supported
    - choice conditions are now supported
    - the new, custom emit dialogue signal node is now supported
    - a new stage/dialogue box for the narrator was added

---
+++ godot/gui/dialogue/NarratorStage.gd
@@ -0,0 +1,50 @@
+extends Control
+
+var text : String setget _set_text, _get_text
+
+var need_to_wait : bool setget _set_need_to_wait
+
+onready var dialogue_text = get_node("%DialogueText")
+
+onready var tween = get_node("Tween")
+
+onready var proceed_indicator = get_node("BoxPanel/ProceedIndicator")
+onready var proceed_indicator_timer = get_node("BoxPanel/ProceedIndicator/Timer")
+
+signal reading_finished
+
+func _set_text(new_val):
+	dialogue_text.bbcode_text = "[center]" + new_val + "[/center]"
+	_animate_dialogue_text()
+
+func _get_text():
+	return dialogue_text.text
+	
+func _set_need_to_wait(new_val):
+	need_to_wait = new_val
+
+func _input(event):
+	if event.is_action_pressed("option_select"):
+		print("enter")
+		emit_signal("reading_finished")
+
+func _on_tween_all_completed():
+	need_to_wait = false
+
+func _ready():
+	need_to_wait = false
+	yield(get_tree().create_timer(1.0), "timeout")
+	proceed_indicator_timer.connect("timeout", self, "_on_ProceedIndicator_switch")
+	tween.connect("tween_all_completed", self, "_on_tween_all_completed")
+
+func _on_ProceedIndicator_switch():
+	proceed_indicator.visible = dialogue_text.percent_visible == 1.0 and not proceed_indicator.visible
+	
+func _on_animation_finished(animation_name):
+	print(animation_name)
+
+func _animate_dialogue_text():
+	tween.stop_all()
+	tween.reset_all()
+	tween.interpolate_property(dialogue_text, "percent_visible", 0.0, 1.0, len(self.text) * 0.01)
+	tween.start()
--- godot/scene/DialogueManager.gd
+++ godot/scene/DialogueManager.gd
@@ -6,36 +6,54 @@ extends Node
 var json_data = {}
 var current_node 
 var local = {} # Stores local variables from the editor
-var box_style : String = "Dialogue"
+var box_style : String = ""
 
 var dialogue_box_scene = preload("res://gui/dialogue/DialogueBox.tscn")
 var choice_box_scene = preload("res://gui/dialogue/ChoiceBox.tscn")
-
+var narrator_stage_scene = preload("res://gui/dialogue/NarratorStage.tscn")
 
 onready var dialogue_box : Control = get_node("DialogueBox")
 onready var choice_box : Control = get_node("ChoiceBox")
+onready var narrator_stage : Control = get_node("NarratorStage")
+
+signal dialogue_signal(command)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	start_dialogue("res://content/dialogue/Intro_Part3.json")
-	dialogue_box.connect("reading_finished", self, "on_reading_finished")
-	remove_child(choice_box)
+	start_dialogue("res://content/dialogue/Chapter2P1.json")
 	handle_current_node()
 	pass # Replace with function body.
 
-func toggle_box():
-	if box_style == "Dialogue":
+func change_box_style(new_style):
+	if box_style == new_style:
+		return
+	
+	box_style = new_style
+	
+	if dialogue_box and dialogue_box.is_inside_tree():
 		remove_child(dialogue_box)
-		choice_box = choice_box_scene.instance()
-		add_child(choice_box)
-		choice_box.connect("choice_selected", self, "on_choice_selected")
-		box_style = "Choice"
-	else:
+		dialogue_box = null
+	if choice_box and choice_box.is_inside_tree():
 		remove_child(choice_box)
-		dialogue_box = dialogue_box_scene.instance()
-		add_child(dialogue_box)
-		dialogue_box.connect("reading_finished", self, "on_reading_finished")
-		box_style = "Dialogue"
+		choice_box = null
+	if narrator_stage and narrator_stage.is_inside_tree():
+		remove_child(narrator_stage)
+		narrator_stage = null
+	
+	match box_style:
+		"Dialogue":
+			dialogue_box = dialogue_box_scene.instance()
+			add_child(dialogue_box)
+			dialogue_box.connect("reading_finished", self, "on_reading_finished")
+		"Choice":
+			choice_box = choice_box_scene.instance()
+			add_child(choice_box)
+			choice_box.connect("choice_selected", self, "on_choice_selected")
+		"Stage":
+			narrator_stage = narrator_stage_scene.instance()
+			add_child(narrator_stage)
+			narrator_stage.connect("reading_finished", self, "on_reading_finished")
+			
 
 func load_data(json_path):
 	var file = File.new()
@@ -86,16 +104,22 @@ func on_reading_finished():
 
 func show_message():
 	if current_node["character"][0] == "Player":
-		if box_style == "Dialogue":
-			toggle_box()
+		change_box_style("Choice")
+		
 		choice_box.question = current_node["text"]["ENG"]
 		var tmp = []
 		for i in range(len(current_node["choices"])):
-			tmp.append(current_node["choices"][i]["text"]["ENG"])
+			if not current_node["choices"][i]["is_condition"] or \
+			 evaluate_value(current_node["choices"][i]["condition"]):
+				tmp.append(current_node["choices"][i]["text"]["ENG"])
 		choice_box.choices = tmp
+	elif current_node["character"][0] == "Narrator":
+		change_box_style("Stage")
+		
+		narrator_stage.text = current_node["text"]["ENG"]
 	else:
-		if box_style == "Choice":
-			toggle_box()
+		change_box_style("Dialogue")
+		
 		dialogue_box.text = current_node["text"]["ENG"]
 		dialogue_box.display_name = current_node["character"][0]
 
@@ -107,33 +131,57 @@ func auto_next():
 
 func handle_current_node():
 	if not current_node:
-		dialogue_box.text = "Next Chapter"#assert(false, "Implement next chapter here")
+		narrator_stage.text = "Next Chapter"#assert(false, "Implement next chapter here")
 	elif current_node["node_type"] == "condition_branch":
 		condition_branch()
 	elif current_node["node_type"] == "show_message":
 		show_message()
 	elif current_node["node_type"] == "set_local_variable":
 		set_local_var()
+	elif current_node["node_type"] == "chance_branch":
+		chance_branch()
+	elif current_node["node_type"] == "dialogue_signal":
+		emit_dialogue_signal()
+
+func emit_dialogue_signal():
+	emit_signal("dialogue_signal", current_node["signal"])
+	
+	auto_next()
+	handle_current_node()
+
+func chance_branch():
+	var chance_1 = current_node["chance_1"]
+	var chance_2 = current_node["chance_2"]
+	
+	var rand_num = randi() % int(chance_1 + chance_2) + 1
+	
+	if rand_num <= chance_1:
+		current_node = get_dialogue_node_by_id(current_node["branches"]["1"])
+	else:
+		current_node = get_dialogue_node_by_id(current_node["branches"]["2"])
+	
+	handle_current_node()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 func condition_branch():
-	var c = current_node["text"]
+	var condition = current_node["text"]
 	
-	var parts : PoolStringArray = c.split(" ")
-	var var_name : String = parts[0]
-	var comparator : String = parts[1]
-	var value : String = parts[2]
-	
-	if evaluate_value(var_name, comparator, value):
+	if evaluate_value(condition):
 		current_node = get_dialogue_node_by_id(current_node["branches"]["True"])
 	else:
 		current_node = get_dialogue_node_by_id(current_node["branches"]["False"])
 	
 	handle_current_node()
 
-func evaluate_value(var_name : String, comparator : String, value : String):
+func evaluate_value(condition : String):
+	var parts : PoolStringArray = condition.split(" ")
+	var var_name : String = parts[0]
+	var comparator : String = parts[1]
+	var value : String = parts[2]
+	
 	var var_value = str(local[var_name]["value"])
 	
 	match comparator:
